641658dc617e96eedcb4742f4f402c8ef0df367d
hiren.kanzariya01 <kanzariyahiren655@gmail.com>
2026-07-10 15:34:22 +0530

    create setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
 from setuptools import setup, find_packages
+from typing import List
 
-def get_requirements(path:str)->list[str]:
-  with open(path) as p:
-    requirements = p.readlines()
-    requirements = [req.replace('\n', '') for req in requirements]
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(path: str) -> List[str]:
+    requirements = []
+    with open(path) as p:
+        requirements = p.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        
+    if HYPEN_E_DOT in requirements:
+        requirements.remove(HYPEN_E_DOT)
+        
     return requirements
 
+
 setup(
-  name='Machine Learning Project',
-  version=0.0.3, 
-  author='Hiren Knzariya',
-  author_email='hirenkanzariya655@gmail.com',
-  packages=find_packages(),
-  install_requires=get_requirements('requirements.txt')
+    name="ML Project",
+    version="0.0.1",
+    author="hiren kanzariya",
+    author_email="kanzariyahiren655@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
 )
