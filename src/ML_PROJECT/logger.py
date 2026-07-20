8a920a701d8fafab43137168c26ebeef04a89f1d
hiren.kanzariya01 <kanzariyahiren655@gmail.com>
2026-07-20 15:28:23 +0530

    add code

--- src/ML_PROJECT/logger.py
+++ src/ML_PROJECT/logger.py
@@ -1,16 +1,16 @@
-from datetime import datetime
-import os
 import logging
+import os
+from datetime import datetime
 
-FILE_NAME = f"{datetime.now().strftime('%m_%d_%Y_%S_%M_%H')}.log"
-FOLDER_NAME = os.path.join(os.getcwd(), "logs")
-os.makedirs(FOLDER_NAME, exist_ok=True)
+LOG_DIR = os.path.join(os.getcwd(), "logs")
+os.makedirs(LOG_DIR, exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(FOLDER_NAME, FILE_NAME)
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[%(asctime)s] %(levelname)-8s | %(name)s | Line:%(lineno)d | %(funcName)s() | %(message)s",
     level=logging.INFO,
+    format="[%(asctime)s] %(levelname)-8s | %(name)s | Line:%(lineno)d | %(funcName)s() | %(message)s",
 )
