a395e4928610b03a2c4d60c62d8dd1424059055a
hiren.kanzariya01 <kanzariyahiren655@gmail.com>
2026-07-20 16:05:34 +0530

    create a custome Exception handler

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from src.ML_PROJECT import logger
+from src.ML_PROJECT.exception import CustomeException
+import sys
 
-if __name__=="__main__":
-  logger.logging.info('this logging file call in app.py')
+try:
+    a = 10
+    b = 0
+    c = a / b
+except Exception as e:
+    logger.logging.info(CustomeException(e, sys))
+    raise CustomeException(e, sys)
---
+++ src/ML_PROJECT/exception.py
@@ -0,0 +1,19 @@
+import sys
+
+def GetErrorDetails(error, error_details: sys):
+    _, _, tb_obj = error_details.exc_info()
+    Line_number = tb_obj.tb_lineno
+    File_name = tb_obj.tb_frame.f_code.co_filename
+
+    error_message = f"Error occurred in Line Number :- {Line_number}, Error occurred in File Name :- {File_name} , Error Message {error}"
+
+    return error_message
+
+
+class CustomeException(Exception):
+    def __init__(self, error, error_message: sys):
+        super().__init__(error_message)
+        self.error_message = GetErrorDetails(error, error_message)
+
+    def __str__(self):
+        return self.error_message
